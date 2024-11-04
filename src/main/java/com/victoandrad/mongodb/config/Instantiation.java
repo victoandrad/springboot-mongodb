89e3e6d87b222c4d60d3af9d9c702ed5908d985a
Victor Andrade <victor64gaming@gmail.com>
2024-11-04 17:12:02 -0300

    Post entity

--- src/main/java/com/victoandrad/mongodb/config/Instantiation.java
+++ src/main/java/com/victoandrad/mongodb/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.victoandrad.mongodb.config;
 
+import com.victoandrad.mongodb.domain.Post;
 import com.victoandrad.mongodb.domain.User;
+import com.victoandrad.mongodb.repositories.PostRepository;
 import com.victoandrad.mongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,14 +18,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         User user1 = new User(null, "Victor Andrade", "victor@gmail.com");
         User user2 = new User(null, "Luiza Aurora", "luiza@gmail.com");
         User user3 = new User(null, "Livia Aurora", "livia@gmail.com");
 
-        userRepository.deleteAll();
         userRepository.saveAll(List.of(user1, user2, user3));
+
+        Post post1 = new Post(null, sdf.parse("03/11/2024"), "Partiu Enem!", "Hoje vou fazer o meu primeiro Enem!", user2);
+        Post post2 = new Post(null, sdf.parse("10/11/2024"), "Partiu Enem Novamente!", "Vamos terminar logo com isso!", user2);
+
+        postRepository.saveAll(List.of(post1, post2));
     }
 }
---
+++ src/main/java/com/victoandrad/mongodb/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.victoandrad.mongodb.repositories;
+
+import com.victoandrad.mongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
